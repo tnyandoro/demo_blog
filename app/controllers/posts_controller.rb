eb81d4b715194776166571fbc865f28df5b1fe2d
Tendai Nyandoro <tnyandoro@gmail.com>
2020-12-01 22:52:45 +0200

    Add the delete method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,14 @@ class PostsController < ApplicationController
 
   def edit
 		@post = Post.find(params[:id])
-	end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
 
    private
 
